f8d62af6cd88aa592fe2eb78ab666f466f75c126
siavash <siavash.kkv@gmail.com>
2021-08-18 00:13:19 +0430

    kut moblie app

--- app/src/main/java/com/android/kutmoblie/MainActivity.java
+++ app/src/main/java/com/android/kutmoblie/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -16,6 +18,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
   ActionBarDrawerToggle actionBarDrawerToggle;
   Toolbar toolbar;
   NavigationView navigationView;
+  FragmentTransaction fragmentTransaction;
+  FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,14 +29,22 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setSupportActionBar(toolbar);
         drawerLayout = findViewById(R.id.drawer);
         navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
- drawerLayout.addDrawerListener(actionBarDrawerToggle);
- actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
-  actionBarDrawerToggle.syncState();
+         drawerLayout.addDrawerListener(actionBarDrawerToggle);
+         actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+         actionBarDrawerToggle.syncState();
+
+  fragmentManager = getSupportFragmentManager();
+  fragmentTransaction = fragmentManager.beginTransaction();
+  fragmentTransaction.add(R.id.item_frag,new mainfrag());
+  fragmentTransaction.commit();
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
         return true;
     }
 }
